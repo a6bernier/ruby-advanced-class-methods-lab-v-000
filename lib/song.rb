74fe6552b87324946b1eddc47898c15e9af01426
Andrew Bernier <a6bernier@gmail.com>
2018-10-15 19:41:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,11 @@ class Song
     self.class.all << self
   end
 
-  def self.create
-    self.class.all << self
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
+
   end
 
 end
