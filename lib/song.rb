6f70ff82a29065e96ffb8dcdc0217603bb08c228
Andrew Bernier <a6bernier@gmail.com>
2018-10-15 21:59:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,5 +58,12 @@ class Song
     song
   end
 
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = filename.split(" - ")[1].split(".")[0]
+    song.artist_name = filename.split(" - ")[0]
+    song
+  end
+
 
 end
