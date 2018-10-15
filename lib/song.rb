6e33498fb29ceaed11d4ec5ddcc4e54fc9634603
Andrew Bernier <a6bernier@gmail.com>
2018-10-15 20:46:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name == name
-      self.find_by_name
+    if find_by_name(name) == name
+      find_by_name
     else
-      self.create_by_name
+      create_by_name(name)
     end
   end
 
